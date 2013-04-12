8aa05be08fd743e46ab3c0210b2fe640bd32dce7
ebakker <ebakker@sce-bio-c03066.bio.ed.ac.uk>
2013-04-12 10:07:53 +0100

    committing before going to matt's code and getting updates that will make old cell vision files unusable.
    At this stage active contour is working reasonably well, and intending to write a GUI for to edit active contours over time.

---
+++ ActiveContourFunctions/+ACImageTransformations/radial_gradient_and_DIC_angle.m
@@ -0,0 +1,117 @@
+function imageStack = radial_gradient(imageStack,parameters,varargin)
+%function image = radial_gradient_DICangle_and_radialaddition(imageStack,parameters,varargin)
+
+%parameters.invert    - whether to invert the final cost function image or
+%                       not (before adding trappiness)
+
+%varargin{1} - image stack of trapiness of each pixel in the corresponding image in imageStack
+
+%NOTES
+
+%With 'invert' false this transformation will give low scores for pixels at
+%which radial change in intensity is positive (from black to white) 
+%With'invert' true it will give low scores for pixels at which radial change in
+%intensity is negative (from white to black)
+
+%% general setting up
+
+image_length = (size(imageStack,1)-1)/2;%half the length of the image
+
+xcoord = repmat(-image_length:image_length,(2*image_length +1),1);
+
+ycoord = repmat((-image_length:image_length)',1,(2*image_length +1));
+
+xcoord((image_length +1),(image_length +1)) = 1;
+
+ycoord((image_length +1),(image_length +1)) = 1;
+
+[R,angle] = ACBackGroundFunctions.xy_to_radial(xcoord(:),ycoord(:));
+
+R = reshape(R,(2*image_length+1),(2*image_length+1));
+
+angle = reshape(angle,(2*image_length+1),(2*image_length+1));
+
+for i=1:size(imageStack,3)
+
+image = imageStack(:,:,i);
+if size(varargin,2)>=1
+    trap_px = varargin{1}(:,:,i);
+end
+
+
+[ximg,yimg] = gradient(image);
+
+%% radial gradient
+image = -ximg.*cos(angle) -yimg.*sin(angle); 
+
+%% DIC angle part
+DICAngleimage = image.*tanh((cos(angle-(parameters.DICangle*2*pi/360))));
+
+
+if isfield(parameters,'postprocessing')
+switch parameters.postprocessing
+    case 'none' 
+        %do nothing
+    case 'invert'
+        %invert the image
+        image = max(image(:))-image;
+    case 'absolute'
+        %take the absolute of the image
+        image = abs(image - median(image(:)));
+        image = max(image(:))-image;
+    case 'absolute+'
+        % negative of (absolute followed by addition of  half the original
+        % image)
+        image = abs(image - median(image(:))) + 0.5*(DICAngleimage - median(DICAngleimage(:)));
+        image = max(image(:))-image;
+    case 'absolute-'
+        %absolute followed by subtraction of  half the original image
+        image = abs(image - median(image(:))) - 0.5*(DICAngleimage - median(DICAngleimage(:)));
+        image = max(image(:))-image;
+end
+end
+
+
+if size(varargin,2)>=1
+   MAXimage = max(abs(image(:)));
+   %trap points between 0 and 1 scaled as per trapiness
+   image = image+((median(image(:))-image).*mod(trap_px,1));
+   %trap points greater than 1 (largely certain) set to max)
+   image(trap_px>4) = MAXimage; 
+   
+   %just for easier image
+   image(image>(2*MAXimage)) = 2*MAXimage;
+   
+   
+
+end
+
+imageStack(:,:,i) = image;
+
+end
+
+end
+% checkparams - nobody got time for that.
+% 
+% function error_message = checkparams(parameters)
+% 
+% if ~islogical(parameters,'invert')
+% error_message = [field ' must be a parameter of this function'];
+% error(['incorrect parameters passed to radial_gradient_DICangle_and_radialaddition.' field ' must be a parameter'])
+% end
+% 
+% if ~islogical(parameters.invert)
+% error_message = ['invert must be logical'];
+% error(['incorrect parameters passed to radial_gradient_DICangle_and_radialaddition. invert must be a single logical'])
+% end
+% 
+% if any(~size((parameters.invert)==[1 1]))
+% error_message = [field ' must be a single number'];
+% error(['incorrect parameters passed to radial_gradient_DICangle_and_radialaddition. invert must be a single number'])
+% end
+% 
+% error_message = [];
+% end
+
+
+
