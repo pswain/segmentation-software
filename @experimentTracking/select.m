971fc885adf83689a1a2dcbd4d8b008659463ef5
mcrane2 <mcrane2@sce-bio-c03067.bio.ed.ac.uk>
2013-04-28 13:07:48 +0100

    added changes to the cExperiment GUI to allow you to select the cells to plot manually

---
+++ @experimentTracking/select.m
@@ -0,0 +1,17 @@
+function select(cExperiment,cCellVision,positionsToIdentify)
+
+
+if nargin<3
+    positionsToIdentify=1:length(cExperiment.dirs);
+end
+    
+%% Load timelapses
+for i=1:length(positionsToIdentify)
+    currentPos=positionsToIdentify(i);
+    load([cExperiment.rootFolder '/' cExperiment.dirs{currentPos},'cTimelapse']);
+    cTrapDisplayPlot(cTimelapse,cCellVision);
+    
+    uiwait();
+    cExperiment.cTimelapse=cTimelapse;
+    cExperiment.saveTimelapseExperiment(currentPos);
+end
