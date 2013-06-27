78061785e3850b36575cf895d48aa4ea44bb4f3e
Elco Bakker <e.bakker@sms.ed.ac.uk>
2013-06-27 16:31:07 +0100

    bug fixes done with matt on the one position GUI timelapseTrapsGUI

--- @timelapseTrapsGUI/identifyCells.m
+++ @timelapseTrapsGUI/identifyCells.m
@@ -3,11 +3,11 @@ function identifyCells(cTrapsGUI)
 answer = inputdlg('Enter twoStage Threshold','TwoStageTreshold',1,{num2str(cTrapsGUI.cCellVision.twoStageThresh)});
 cTrapsGUI.cCellVision.twoStageThresh=str2double(answer{1});
 
-answer = inputdlg('Track traps again?','TrackAgain',1,{0});
+answer = inputdlg('Track traps again?','TrackAgain',1,{'0'});
 trackTrapsOverwrite=str2double(answer{1})>0;
 
 if trackTrapsOverwrite
-    cTimelapse.trackTrapsThroughTime(cCellVision,cExperiment.timepointsToProcess);
+    cTrapsGUI.cTimelapse.trackTrapsThroughTime(cTrapsGUI.cCellVision);
 end
 
 cTrapDisplayProcessing(cTrapsGUI.cTimelapse,cTrapsGUI.cCellVision);
