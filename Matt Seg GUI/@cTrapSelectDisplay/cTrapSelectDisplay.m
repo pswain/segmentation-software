d99a71afdf61080e2b2bc4d9a5b48222f6337edd
Elco Bakker <e.bakker@sms.ed.ac.uk>
2014-04-30 09:55:32 +0100

    changed identify traps of cExperiment to use the first timepoint that you want to process rather than timepoint 1.
    changed the track traps so that it reset the 'base image' (one to which it compares to identify drift) when the distance drifted was more than half the trap image size. Helps in very large total drift cases.
    
    Added new feature to cExperiment trap selection method such that you can choose to track the first position and use this to exclude from the auto find traps that would move out of the frame due to drift.

--- Matt Seg GUI/@cTrapSelectDisplay/cTrapSelectDisplay.m
+++ Matt Seg GUI/@cTrapSelectDisplay/cTrapSelectDisplay.m
@@ -9,13 +9,15 @@ classdef cTrapSelectDisplay<handle
         trapLocations=[];
         cCellVision=[];
         channel=[];
+        ExclusionZones = []; %zones in which to not look for traps automatically stored as 4 vector [xStart1 yStart1 xend1 yend1;xStart2 yStart2 xend2 yend2]
+        
     end % properties
     %% Displays timelapse for a single trap
     %This can either dispaly the primary channel (DIC) or a secondary channel
     %that has been loaded. It uses the trap positions identified in the DIC
     %image to display either the primary or secondary information.
     methods
-        function cDisplay=cTrapSelectDisplay(cTimelapse,cCellVision,timepoint,channel)
+        function cDisplay=cTrapSelectDisplay(cTimelapse,cCellVision,timepoint,channel,ExclusionZones)
             
             if isempty(cCellVision.cTrap)
                 errordlg('This cCellVision Model was made to work for timelapses without traps');
@@ -27,28 +29,63 @@ classdef cTrapSelectDisplay<handle
                 return;
             end
             
-            if nargin<3
+            if nargin<3 || isempty(timepoint)
                 timepoint=1;
             end
             cDisplay.timepoint=timepoint;
             
-            if nargin<4
+            if nargin<4 || isempty(channel)
                 cDisplay.channel=1;
             else
                 cDisplay.channel=channel;
             end
+            
+            if nargin<5 || isempty(ExclusionZones)
+                cDisplay.ExclusionZones = [];
+            else
+                cDisplay.ExclusionZones = ExclusionZones;
+            end
+            
             cDisplay.cCellVision=cCellVision;
             cDisplay.cTimelapse=cTimelapse;
             cDisplay.figure=figure;
             cDisplay.axesHandle=axes();
             set(cDisplay.axesHandle,'xtick',[],'ytick',[])
             cDisplay.trapLocations=cTimelapse.cTimepoint(timepoint).trapLocations;
+            PreExistingTrapLocations = cTimelapse.cTimepoint(timepoint).trapLocations;
             
             cDisplay.image=cTimelapse.returnSingleTimepoint(timepoint,cDisplay.channel);
             
             [cDisplay.trapLocations trap_mask]=cTimelapse.identifyTrapLocationsSingleTP(timepoint,cCellVision,cDisplay.trapLocations);
 
+            TrapsToRemove = [];
+            for trapi = 1:length(cDisplay.trapLocations)
+                for zonei = 1:size(cDisplay.ExclusionZones,1)
+                    
+                    if cDisplay.trapLocations(trapi).xcenter>=cDisplay.ExclusionZones(zonei,1) && ...
+                            cDisplay.trapLocations(trapi).xcenter<=cDisplay.ExclusionZones(zonei,3) && ...
+                            cDisplay.trapLocations(trapi).ycenter>=cDisplay.ExclusionZones(zonei,2) && ...
+                            cDisplay.trapLocations(trapi).ycenter<=cDisplay.ExclusionZones(zonei,4);
+                        TrapsToRemove = [TrapsToRemove trapi];
+                    end
+                end
+                
+            end
+            cDisplay.trapLocations(TrapsToRemove) = [];
             
+            %bit ugly, but don't want to remove traps that are in the
+            %ctimelapse already since they may have been added by user.
+            if ~isempty(PreExistingTrapLocations)
+            [~,TrapsToPutBack] = setdiff([[PreExistingTrapLocations(:).xcenter]' [PreExistingTrapLocations(:).ycenter]'],...
+                    [[cDisplay.trapLocations(:).xcenter]' [cDisplay.trapLocations(:).ycenter]'],'rows');
+            
+                for trapi = TrapsToPutBack'
+                    cDisplay.trapLocations(end+1) = cTimelapse.cTimepoint(timepoint).trapLocations(trapi);
+                end
+            end
+            
+            [cDisplay.trapLocations trap_mask]=cDisplay.cTimelapse.identifyTrapLocationsSingleTP(cDisplay.timepoint,cDisplay.cCellVision,cDisplay.trapLocations);
+                
             im_mask=cDisplay.image;
             im_mask(trap_mask)=im_mask(trap_mask)*1.5;
             cDisplay.imHandle=imshow(im_mask,[],'Parent',cDisplay.axesHandle);
@@ -61,7 +98,7 @@ classdef cTrapSelectDisplay<handle
         
         function addRemoveTraps(cDisplay)
             cp=get(cDisplay.axesHandle,'CurrentPoint');
-            cp=round(cp)
+            cp=round(cp);
             Cx=cp(1,1);
             Cy=cp(1,2);
             
