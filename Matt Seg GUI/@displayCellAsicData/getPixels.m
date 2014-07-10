a5d7d715a88bc6a12631d26db46f8900bc0d6056
Calum Tollervey <ctollervey@gmail.com>
2014-07-10 13:11:46 +0100

    Temp fix for a bug where the segmented circle would have gaps in it, leading imfill to not fill it correctly and so not get correct statistics
    Fixed by adding second circle with radius 1px less than segmented. Ugly solution but seems to work for now.

--- Matt Seg GUI/@displayCellAsicData/getPixels.m
+++ Matt Seg GUI/@displayCellAsicData/getPixels.m
@@ -14,34 +14,35 @@ cellID=cData.highlightedCell;
 cTimelapse=cData.cTimelapse;
 cellNum=find(cTimelapse.cTimepoint(timepoint).trapInfo.cellLabel==cellID);
 outlines=cTimelapse.cTimepoint(timepoint).trapInfo.cell(cellNum).segmented;
-%Generate second (outer) circle to ensure there are no gaps in the cell
-%outline
-rad=cTimelapse.cTimepoint(timepoint).trapInfo.cell(cellNum).cellRadius; 
-center=cTimelapse.cTimepoint(timepoint).trapInfo.cell(cellNum).cellCenter;
-theta=0:0.01:2*pi;
-coords=[double(center(2))+(rad+1)*cos(theta); double(center(1))+(rad+1)*sin(theta)];
-innerline=zeros(512,512);
+% %Generate second (outer) circle to ensure there are no gaps in the cell
+% %outline
+% rad=cTimelapse.cTimepoint(timepoint).trapInfo.cell(cellNum).cellRadius;
+% center=cTimelapse.cTimepoint(timepoint).trapInfo.cell(cellNum).cellCenter;
+% theta=0:0.01:2*pi;
+% coords=[double(center(2))+(rad+1)*cos(theta); double(center(1))+(rad+1)*sin(theta)];
+% innerline=zeros(512,512);
 
-for i=1:length(coords(1,:))
-    innerline(floor(coords(1,i)),floor(coords(2,i)))=1;
-end
-innerline=innerline(1:512,1:512); %snip off the ends
-outlines=outlines+innerline;
+% for i=1:length(coords(1,:))
+%     innerline(floor(coords(1,i)),floor(coords(2,i)))=1;
+% end
+% innerline=innerline(1:512,1:512); %snip off the ends
+%outlines=outlines+innerline;
 outlinefilled=imfill(full(outlines),'holes');
-
 [ypos,xpos]=find(full(outlines));
+
+
 ymax=max(ypos);ymin=min(ypos);xmax=max(xpos);xmin=min(xpos);
 imageIn=imread([cTimelapse.timelapseDir filesep cTimelapse.cTimepoint(timepoint).filename{2}]);
 imageIn=double(imageIn)/double(max(imageIn(:)));
 procImage=imageIn;
-procImage(outlinefilled==0)=0;
+procImage(outlinefilled==0)=NaN;
 procImage=procImage(ymin:ymax,xmin:xmax);
 %figure('name',['Cell ' num2str(cellID) ', T' num2str(timepoint)]); imshow(procImage,[]);
 procImage=flipud(procImage);
 if isempty(cData.individualCellFigure)
     cData.individualCellFigure=figure('name',['Cell ' num2str(cellID) ', T' num2str(timepoint)]);
     [~, b]=contourf(procImage);
-    cData.contourAxes=get(b,'parent');
+    cData.contourAxes=get(b,'parent'); %High level functions are annoying and tempremenatl
     caxis([0 1]);
     colorbar;
 else
@@ -56,7 +57,9 @@ hold(cData.contourAxes,'on');
 
 
 %add max5 and median markers
-imagesort=sort(procImage(:),'descend');
+pixels=reshape(procImage,[numel(procImage),1]);
+pixels(isnan(pixels))=[];
+imagesort=sort(pixels,'descend');
 max5=imagesort(1:5);
 med=median(imagesort);
 disp(max5);
--- Matt Seg GUI/@timelapseTraps/extractCellData.m
+++ Matt Seg GUI/@timelapseTraps/extractCellData.m
@@ -108,12 +108,26 @@ for channel=1:length(cTimelapse.channelNames)
                     
                     dataInd=dataInd+1;
                     if isempty(temp_loc)
+                        %Do nothing 
                     else
                         
                         seg_areas=full(trapInfo(currTrap).cell(temp_loc).segmented);
                         segLabel=zeros(size(seg_areas));
                         loc=double(cTimelapse.cTimepoint(timepoint).trapInfo(currTrap).cell(temp_loc).cellCenter);
                         if ~isempty(loc)
+                            %Generate second (outer) circle to ensure there are no gaps in the cell
+                            rad=cTimelapse.cTimepoint(timepoint).trapInfo(currTrap).cell(temp_loc).cellRadius;
+                            center=cTimelapse.cTimepoint(timepoint).trapInfo(currTrap).cell(temp_loc).cellCenter;
+                            theta=0:0.01:2*pi;
+                            coords=[double(center(2))+double(rad+1)*cos(theta); double(center(1))+double(rad+1)*sin(theta)];
+                            innerline=zeros(512,512);
+
+                            for i=1:length(coords(1,:))
+                                innerline(floor(coords(1,i)),floor(coords(2,i)))=1;
+                            end
+                            innerline=innerline(1:512,1:512); %snip off the ends
+                            seg_areas=seg_areas+innerline;
+
                             segLabel=imfill(seg_areas(:,:,1),'holes');%sub2ind(size(seg_areas(:,:,1)),loc(2),loc(1)));
                         end
                         %                 temp_im=trapInfo(currTrap).trackLabel==currCell;
