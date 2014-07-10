3f1122a5a3653391357acaff039b5ab3a691ec2e
Alejandro G <alejandrog@sce-bio-c03675.bio.ed.ac.uk>
2014-07-10 12:23:58 +0100

    Blah

--- GeneralFunctions/process_cExperiments.m
+++ GeneralFunctions/process_cExperiments.m
@@ -4,7 +4,14 @@
 t=tic;
 cExperiment.trackCells(find(cExperiment.posSegmented),5);
 
-cExperiment.combineTracklets(find(cExperiment.posSegmented));
+params.fraction=.1; %fraction of timelapse length that cells must be present or
+params.duration=3; %number of frames cells must be present
+params.framesToCheck=length(cTimelapse.timepointsProcessed);
+params.framesToCheckEnd=1;
+params.endThresh=2; %num tp after end of tracklet to look for cells
+params.sameThresh=4; %num tp to use to see if cells are the same
+params.classThresh=3.8; %classification threshold
+cExperiment.combineTracklets(find(cExperiment.posSegmented),params);
 
 params.fraction=.8; %fraction of timelapse length that cells must be present or
 params.duration=3;%length(cTimelapse.cTimepoint); %number of frames cells must be present
@@ -15,11 +22,12 @@ cExperiment.selectCellsToPlotAutomatic(find(cExperiment.posTracked),params);
 cExperiment.extractCellInformation(find(cExperiment.posTracked),'max');
 
 cExperiment.compileCellInformation;
+% cExperiment.compileCellInformationParamsOnly; %When selecting 'basic'
 
-params.motherDurCutoff=(.6);
-params.motherDistCutoff=2.1;
-params.budDownThresh=0;
-params.birthRadiusThresh=8;
+params.motherDurCutoff=(.6); %fraction of timelapse the mother is present
+params.motherDistCutoff=2.1; %distance from center of dauther to center o mother in radius units. 
+params.budDownThresh=0; %
+params.birthRadiusThresh=8; %cell has to be smaller than this
 params.daughterGRateThresh=-1;
 cExperiment.extractLineageInfo(find(cExperiment.posTracked),params);
 
@@ -31,7 +39,7 @@ cExperiment.extractHMMTrainingStates;
 cExperiment.trainBirthHMM;
 
 cExperiment.classifyBirthsHMM;
-
+%%
 cExperiment.correctSkippedFramesInf
 
 filtParams.num=24;
