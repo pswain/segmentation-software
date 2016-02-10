c26ce9779fb8a99e33ff0ea92e7535c6b1dea9e5
Matthew Crane <nomad.crane@gmail.com>
2016-02-10 13:22:01 -0800

    Removed Scot

--- Scot/+edittimelapse/EditTimelapse.m
+++
@@ -1,9 +0,0 @@
-classdef EditTimelapse<MethodsSuperClass
-    properties
-        resultImage='Result'
-    end
-    methods
-        [timepointObj]=run(obj,timepointObj);
-        timepointObj=initializeFields(obj, timepointObj);
-    end
-end
