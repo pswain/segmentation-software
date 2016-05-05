af1b238a355036acb8bcd7e1458e7778a0b3e927
Lucia Bandiera <Lucia@fuday.bio.ed.ac.uk>
2016-05-05 16:20:05 +0100

    load from experimentTrackingGUI now checks the Path form which the file was loaded and asks the user if they want to change the saveFolder to the path if they do not match.

--- Matt Seg GUI/@experimentTrackingGUI/loadSavedExperiment.m
+++ Matt Seg GUI/@experimentTrackingGUI/loadSavedExperiment.m
@@ -4,9 +4,29 @@ function loadSavedExperiment(cExpGUI)
 l1 = load(fullfile(PathName,FileName));
 cExpGUI.cExperiment=l1.cExperiment;
 
+answer = 'No';
+if ~strcmp(cExpGUI.cExperiment.saveFolder,PathName(1:(end-1)))
+    answer = questdlg('The save folder from which this file was loaded does not match the save location of the cExperiment. Would you like to make them match? (If you have no idea what this means, press ''yes'')','change saveFolder');
+    switch answer
+        case 'Yes'
+            cExpGUI.cExperiment.saveFolder = PathName(1:(end-1));
+        case 'Cancel'
+            fprintf('\n\n    Experiment loading cancelled')
+            cExpGUI.cExperiment = [];
+        return
+end
+
+end
+
+
 if isfield(l1,'cCellVision')
     cExpGUI.cCellVision= l1.cCellVision;
     cExpGUI.cExperiment.cCellVision=l1.cCellVision;
+end
+
+switch answer
+    case 'Yes'
+        cExpGUI.cExperiment.saveExperiment;
 end
 
 
