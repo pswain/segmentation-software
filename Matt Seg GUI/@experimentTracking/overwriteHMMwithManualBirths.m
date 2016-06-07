a8e5a9e5c5a9efd92971521d42bd05b775d181c6
Matthew Crane <nomad.crane@gmail.com>
2016-06-07 13:37:53 -0700

    Small changes to the HMM overwrite so that it works properly

--- Matt Seg GUI/@experimentTracking/overwriteHMMwithManualBirths.m
+++ Matt Seg GUI/@experimentTracking/overwriteHMMwithManualBirths.m
@@ -20,19 +20,33 @@ mTrap=cExperiment.lineageInfo.motherInfo.motherTrap;
 manTrap=cExperiment.lineageInfo.motherInfo.manualInfo.trapNum;
 manPos=cExperiment.lineageInfo.motherInfo.manualInfo.posNum;
 
+if isempty(cExperiment.lineageInfo.motherInfo.birthTimeManual)
+    cExperiment.lineageInfo.motherInfo.birthTimeManual=cExperiment.lineageInfo.motherInfo.manualInfo.oldManualBT;
+end
 for trapInd=1:length(mTrap)
     currTrap=manTrap(trapInd);
     currPos=manPos(trapInd);
     
-    bTManual=cExperiment.lineageInfo.motherInfo.birthTimeManual(trapInd,:);
+    try
+        bTManual=cExperiment.lineageInfo.motherInfo.birthTimeManual(trapInd,:);
+    catch
+        b=1;
+    end
     
     loc = find(cExperiment.lineageInfo.motherInfo.motherTrap== currTrap ...
         & cExperiment.lineageInfo.motherInfo.motherPosNum == currPos);
     if ~isempty(loc)
-        cExperiment.lineageInfo.motherInfo.birthTimeHMM(loc,1:length(bTManual))=bTManual;
+        try
+            bTManual;
+            cExperiment.lineageInfo.motherInfo.birthTimeHMM(loc(1),1:length(bTManual))=bTManual;
+        catch
+            b=1;
+        end
     end
 end
 cExperiment.lineageInfo.motherInfo.manualInfo.oldManualBT=cExperiment.lineageInfo.motherInfo.birthTimeManual;
 cExperiment.lineageInfo.motherInfo.birthTimeManual=[];
 
-cExperiment.saveExperiment;
+try
+    cExperiment.saveExperiment;
+end
