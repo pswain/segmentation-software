93e809349f14ea4d1d76db8d447a0e089039ab8c
Matthew Crane <nomad.crane@gmail.com>
2016-06-22 12:53:53 +0100

    small change to correct bug in alignExpressiontToStart

--- GeneralFunctions/mattDataProcessing/alignExpressionToStart.m
+++ GeneralFunctions/mattDataProcessing/alignExpressionToStart.m
@@ -2,11 +2,12 @@ function cVal=alignExpressionToStart(cVal)
 cVal=full(cVal);
 cVal(cVal==0)=NaN;cVal(cVal==Inf)=NaN;
 tpNo=all(isnan(cVal));
+tpNo=tpNo | all(isinf(cVal));
 cVal=cVal(:,~tpNo);
 
 for i=1:size(cVal,1)
     temp=cVal(i,:);
-    if isnan(temp(1))
+    if false%isnan(temp(1)) || isinf(temp(1))
         loc=find(~isnan(temp),1);
         temp(1:loc-1)=[];
 %         temp=temp/temp(1);
