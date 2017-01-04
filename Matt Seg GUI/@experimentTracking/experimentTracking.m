4645596e32db4505e75ad254c70dc1e86da828d0
SCE-BIO-C03429\elcob_000 <s1135844@ed.ac.uk>
2017-01-04 17:12:18 +0000

    changed experimentTracking to remove Omero code.

--- Matt Seg GUI/@experimentTracking/experimentTracking.m
+++ Matt Seg GUI/@experimentTracking/experimentTracking.m
@@ -1,15 +1,18 @@
 classdef experimentTracking<handle
-    %class for organising and numerous timelapseTraps objects, one for each
-    %position in the experiment. Mostly used to apply identical processing
-    %steps to each position, organise loading and saving of them, and
-    %compile the data from all the separate positions in one location.
-    %Indiviual timelapseTraps objects are created either from local files
-    %or from the omero database.
+    % class for organising and numerous timelapseTraps objects, one for each
+    % position in the experiment. Mostly used to apply identical processing
+    % steps to each position, organise loading and saving of them, and
+    % compile the data from all the separate positions in one location.
+    % Indiviual timelapseTraps objects are created from the folders in the
+    % rootFolder and saved, with the experimentTracking object, in the
+    % saveFolder. 
+    % A subclass (experimentTrackingOmero) is available to load/save/access images 
+    % from the swainlab omeroDataBase.
+    %
+    % See also EXPERIMENTTRACKINGGUI,TIMELAPSETRAPS,EXPERIMENTTRACKINGOMERO
     
     properties
         rootFolder %folder where images are. When images are held in an Omero database this property is the suffix defining the filename: cExperiment_SUFFIX.mat
-        omeroDs%Omero dataset object - alternative source to rootFolder
-        OmeroDatabase%Omero database object
         creator%string, the user who created this object(obtained by getenv('USERNAME'))
         saveFolder %folder to save the timelapse objects
         dirs % cell array of directories in rootFolder. 
@@ -68,14 +71,23 @@ classdef experimentTracking<handle
     
     methods
         
-        function cExperiment=experimentTracking(folder,saveFolder, OmeroDatabase,  expName)
-            %% Folder input is a string with the full path to the root folder or an Omero dataset object (in that case the OmeroDatabase object must also be input)
-            %Optional inputs 2-4 only used when creating objects using the
-            %Omero database: OmeroDatabase - object of class OmeroDatabase
-            %expName, a unique name for this cExperiment
-            % if folder is the logical true then this is a queue to loada
-            % 'bare' cExperiment (i.e. with no defined fields) or user
-            % inputs. Used in loadobj method.
+        function cExperiment=experimentTracking(rootFolder,saveFolder, OmeroDatabase,  expName)
+            %cExperiment=experimentTracking(rootFolder,saveFolder, OmeroDatabase,  expName)
+            % 
+            % INPUTS 
+            % rootFolder   -  EITHER :
+            %                   - a boolean (true) indicating that a bare
+            %                     experimentTracking object should be
+            %                     created (used in loading and subclasses)
+            %                 OR:
+            %                   - a string with the full path to the root
+            %                     folder (i.e. folder where all the
+            %                     postition folders are). 
+            %                 If empty, rootFolder is selected by user
+            %                 input.
+            % saveFolder   -  string. Full path to the folder where the
+            %                 experimentTracking object and created
+            %                 timelapseTraps objects should be saved.
             
             % Create a new logger to log changes for this cExperiment:
             %disp('Not generating log files now - to change open experimentTracking.m')
@@ -85,8 +97,8 @@ classdef experimentTracking<handle
             % Initialise source (root folder) and save paths
             if nargin<1
                 fprintf('\n   Select the Root of a single experimental set containing folders of multiple positions \n');
-                folder=uigetdir(pwd,'Select the Root of a single experimental set containing folders of multiple positions');
-            elseif islogical(folder) && folder
+                rootFolder=uigetdir(pwd,'Select the Root of a single experimental set containing folders of multiple positions');
+            elseif islogical(rootFolder) && rootFolder
                 % if folder is true, cExperiment returned bare for load
                 % function.
                 return
@@ -94,36 +106,12 @@ classdef experimentTracking<handle
             
             if nargin<2
                 fprintf('\n   Select the folder where data should be saved \n');
-                saveFolder=uigetdir(folder,'Select the folder where data should be saved');
+                saveFolder=uigetdir(rootFolder,'Select the folder where data should be saved');
             end            
-            if ischar(folder)
-                cExperiment.rootFolder=folder;
-                cExperiment.saveFolder=saveFolder;
-            else%Experiment is being initialized from an Omero dataset - folder is an omero.model.DatasetI object
-                if nargin>3
-                    if iscell(expName)
-                        expName=expName{:};
-                    end
-                    cExperiment.rootFolder=expName;
-                else
-                    cExperiment.rootFolder='001';%default experiment name
-                end
-                %Define paths for temporary storage of Omero information
-                if ismac
-                    cExperiment.saveFolder=['/Users/' char(java.lang.System.getProperty('user.name')) '/Documents/OmeroTemp'];
-                    
-                else
-                    cExperiment.saveFolder=['C:\Users\' getenv('USERNAME') '\OmeroTemp'];
-                end
-                %Ensure the save folder exists and is empty
-                if ~exist(cExperiment.saveFolder,'dir')
-                      mkdir(cExperiment.saveFolder);
-                end
-                delete([cExperiment.saveFolder '/*']);
-                %Define the Omero properties of cExperiment
-                cExperiment.omeroDs=folder;
-                cExperiment.OmeroDatabase=OmeroDatabase;
-            end
+
+            cExperiment.rootFolder=rootFolder;
+            cExperiment.saveFolder=saveFolder;
+            
             %Record the user who is creating the cExperiment
             if ispc
                 cExperiment.creator=getenv('USERNAME');
@@ -135,13 +123,12 @@ classdef experimentTracking<handle
             cExperiment.posTracked=0;
             %Define the source folders (or Omero image names) for each
             %position
-            if isempty(cExperiment.OmeroDatabase)
-                tempdir=dir(cExperiment.rootFolder);
-            else
-                tempdir=[];
-            end
+
+            tempdir=dir(cExperiment.rootFolder);
+
             cExperiment.dirs=cell(1);
-            if isempty(cExperiment.OmeroDatabase)
+            % created dirs - the list of positions - as the directories in
+            % the rootFolder
                 index=1;
                 for i=1:length(tempdir)
                     if tempdir(i).isdir
@@ -152,67 +139,15 @@ classdef experimentTracking<handle
 
                     end
                 end
-            else
-                %Position list consists of the Omero image names
-                %Make sure the image list is loaded
-                cExperiment.omeroDs=getDatasets(OmeroDatabase.Session,cExperiment.omeroDs.getId.getValue, true);
-                %Get the image list
-                oImages=cExperiment.omeroDs.linkedImageList;
-                for i=1:oImages.size
-                    cExperiment.dirs{i}=char(oImages.get(i-1).getName.getValue);
-                end
-                cExperiment.dirs=sort(cExperiment.dirs);
-            end
+
+            
             cExperiment.cellsToPlot=cell(1);
             cExperiment.ActiveContourParameters = timelapseTrapsActiveContour.LoadDefaultParameters;
             
             %Parse the microscope acquisition metadata and attach the structure to the
             %cExperiment object - this populates the metadata field of
             %cExperiment
-            if ~isempty(cExperiment.OmeroDatabase)
-            %This is an Omero cExperiment - need to download the log and
-            %acq files from the database before parsing
-                expName=char(folder.getName.getValue);
-                logName=[expName(1:end-3) 'log.txt'];
-                acqName=[expName(1:end-3) 'Acq.txt'];
-                logPath=cExperiment.OmeroDatabase.downloadFile(folder,logName);
-                [~]=cExperiment.OmeroDatabase.downloadFile(folder,acqName);
-                cExperiment.parseLogFile(logPath);
-            else
-                cExperiment.parseLogFile;
-            end
-             
-            
-            %Set the channels
-            if ~isempty(cExperiment.OmeroDatabase)                
-                %Set the channels field - add a separate channel for each section in case
-                %they are required for data extraction or segmentation:
-                %Get the number of Z sections
-                im=oImages.get(0);%the first image - assumes all images have the same dimensions
-                pixels=im.getPrimaryPixels;
-                sizeZ=pixels.getSizeZ.getValue;
-                %Get the list of channels from the microscope log
-                origChannels=cExperiment.metadata.acq.channels.names;
-                cExperiment.OmeroDatabase.MicroscopeChannels = origChannels;
-                cExperiment.experimentInformation.MicroscopeChannels=origChannels;
-                %The first entries in
-                %cExperiment.experimentInformation.channels are the
-                %original channels - then followed by a channel for each
-                %section.
-                if sizeZ>1
-                    for ch=1:length(origChannels)
-                        if cExperiment.metadata.acq.channels.zsect(ch)==1%Does the channel do z sectioning?                        
-                            for z=1:sizeZ
-                                if cExperiment.metadata.acq.channels.zsect(ch)==1
-                                    cExperiment.OmeroDatabase.Channels{length(cExperiment.OmeroDatabase.Channels)+1}=[origChannels{ch} '_' num2str(z)];
-                                end
-                            end
-                        end
-                    end
-                end
-                cExperiment.experimentInformation.channels=cExperiment.OmeroDatabase.Channels;
-
-            end
+            cExperiment.parseLogFile;
             
         end
         
@@ -221,6 +156,10 @@ classdef experimentTracking<handle
     methods(Static)
 
         function cExperiment = loadobj(LoadStructure)
+            % cExperiment = loadobj(LoadStructure)
+            % load function to help maintain back compatability and take
+            % care of fiddly loading behaviour (such as logger)
+            
             
             %% default loading method: DO NOT CHANGE
             cExperiment = experimentTracking(true);
@@ -240,6 +179,7 @@ classdef experimentTracking<handle
                 
             end
             
+            %% addtional stuff for back compatability etc.
             
             % Create a new experimentLogging object when loading:
             if ~cExperiment.shouldLog
